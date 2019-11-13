7e8415cb1399ce865f88017b2fdc06914be306c1
Victor Carpenter <victorcarpenter123@gmail.com>
2019-11-13 11:55:45 -0500

    init app

---
+++ components/footer.vue
@@ -0,0 +1,7 @@
+<template>
+  <section>
+    <h1>OOP</h1>
+  </section>
+</template>
+<script></script>
+<style></style>
---
+++ components/header.vue
@@ -0,0 +1,22 @@
+<template>
+  <section class="c-header">
+    <Logo class="c-header__logo" />
+  </section>
+</template>
+<script>
+import Logo from '~/components/Logo.vue'
+
+export default {
+  components: {
+    Logo
+  }
+}
+</script>
+<style>
+.c-header {
+  padding: 20px;
+}
+.c-header__logo {
+  width: 50px;
+}
+</style>
