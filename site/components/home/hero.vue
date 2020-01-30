e62b4b8e3ca05f77c3a8e2fe798e13261434f2a8
Victor Carpenter <victorcarpenter123@gmail.com>
2020-01-29 19:20:02 -0500

    hero done

--- site/components/home/hero.vue
+++ site/components/home/hero.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="hero">
-    <div></div>
+    <div class="foreground"><h1>Find what<br/>Moves you</h1></div>
     <div class="background">
       <div class="row row1">
         <span class="shoe"><img :src="require('@/assets/vulcan.png')" /></span>
@@ -44,26 +44,24 @@
         gsap.set(`.row${i} .shoe`, {
 
           x:(b)=>{
-            return ((b * 250) ) + (i * 500);
+            return b * 250;
           }
         })
-
+        // let myx
+        // if(i == 2){myx = '-=2000'}else{myx = '+=2000'}
         gsap.to(`.row${i} .shoe`, 50, {
           ease: 'none',
-          x: '+='+(500 + (i*700)), //move each box 500px to right
+          x: '+=2000', //move each box 500px to right
           modifiers: {
             x:(x)=>{
               let rt = parseInt(x) % 2000
-
-              if(rt <= 0) rt = 1999
-
               return rt + 'px'
             }
           },
           repeat: -1
         })
       }
-    }
+    },
   }
 </script>
 <style>
@@ -72,7 +70,8 @@
     height:calc(100vh - 80px);
     background:#111;
     position: relative;
-    max-height: 720px;
+    max-height: 700px;
+    overflow:hidden;
   }
 
   .row{
@@ -85,19 +84,37 @@
   }
 
   .background{
-    overflow: hidden;
-    opacity: 0.5;
+    transform: rotateZ(20deg);
+    opacity: 0.4;
   }
 
   .shoe{
     position:absolute;
     width:200px;
   }
-  /* .row2 .shoe img{
-    transform: rotateY(180deg);
-  } */
+  .row2 {
+    transform: translate(-50%,0) rotateY(180deg);
+  }
   .shoe img{
     width: 100%;
+  }
+
+  .foreground{
+    position: absolute;
+    z-index: 1;
+    left: 20%;
+    top: 50%;
+    transform: translate(-50%,-50%);
+  }
+  .foreground h1{
+    font-size: 60px;
+    font-size: 72px;
+    color:White;
+    text-transform: uppercase;
+    letter-spacing: 1px;
+    background: -webkit-linear-gradient(#eee, #555);
+     -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
   }
 
 </style>
